76a4fabb6ed0981881f6f46e94be13f991a60489
AJK-dev <>
2018-09-27 14:22:53 +0200

    Adding scipy

--- python/manifests/init.pp
+++ python/manifests/init.pp
@@ -68,7 +68,7 @@ class python {
 
     $pip_packages = ["ipython", "django", "django-debug-toolbar", "psycopg2", "biopython", "xlrd", "numpy", "PyYAML",
         "djangorestframework", "django-rest-swagger", "XlsxWriter", "sphinx", "openpyxl", "xmltodict", "pandas",
-	"django-polymorphic", "mmtf-python"]
+	"django-polymorphic", "mmtf-python", "scipy"]
     puppet::install::pip { $pip_packages: }
 
         # download and install dssp
