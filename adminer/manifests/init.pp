17cb38185a45bd71288837b8e2a7c2178bb65dfd
Christian Munk <christian@munk.be>
2020-01-09 10:20:00 +0100

    Use latest adminer.php instead of installing adminer via apt-get. Requires changing the install slightly to include php/pgsql .

--- adminer/manifests/init.pp
+++ adminer/manifests/init.pp
@@ -2,8 +2,10 @@ class adminer {
 
     # package install list
     $packages = [
+        "libapache2-mod-php",
+        "php-pgsql",
         "apache2",
-        "adminer",
+        "php",
     ]
 
     # install packages
@@ -11,13 +13,16 @@ class adminer {
         ensure => present,
         require => Exec["update-package-repo"]
     }
+    
+    file { '/var/www/html/adminer/':
+        ensure => 'directory',
+        require => Package["apache2"],
+    }
 
-    # configure adminer
-    file { "/etc/apache2/conf-enabled/adminer.conf":
-        ensure => link,
-        target => "/etc/adminer/apache.conf",
-        require => Package["adminer"],
-        notify => Service["apache2"],
+    exec { "fetch-adminer":
+        command => "wget http://www.adminer.org/latest.php -O /var/www/html/adminer/index.php",
+        creates => "/var/www/html/adminer/index.php",
+        require => Package["apache2"],
     }
 
     # starts the apache2 service once the packages installed, and monitors changes to its configuration files and
